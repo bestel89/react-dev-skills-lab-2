6a18c2a92baa11cc90acfa17102c3b0fc345792e
bestel89 <ben.stell1989@gmail.com>
2023-06-29 14:09:59 +0100

    end of lab 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import NewSkillForm from './components/NewSkillForm'
-import SkillList from './components/SkillList'
+//Import assets
 import "./styles.css"
 
+//Import useState
+import { useState } from 'react'
+
+//Import custom components
+import NewSkillForm from './components/NewSkillForm'
+import SkillList from './components/SkillList'
+
 function App() {
+
+    //! State
+    const [skills, setSkills] = useState([
+        { name: "HTML", level: 5 },
+        { name: "CSS", level: 3 },
+        { name: "JavaScript", level: 4 },
+        { name: "Python", level: 2 },
+    ])
+
+    //! JSX
     return (
-        <>
         <div className="App teal-text">
             <h1>React Dev Skills</h1>
             <SkillList skills={skills} />
             <hr></hr>
-            <NewSkillForm />
+            <NewSkillForm skills={skills} setSkills={setSkills}/>
         </div>
-        </>
     )
 }
-
-const skills = [
-    { name: "HTML", level: 5 },
-    { name: "CSS", level: 3 },
-    { name: "JavaScript", level: 4 },
-    { name: "Python", level: 2 },
-]
 
 export default App
